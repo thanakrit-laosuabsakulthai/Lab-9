4e933bc540da85b0ff9a740ffd08cecf99495a75
Thanakrit <thanakrit_la@cmu.ac.th>
2025-12-19 15:44:10 +0700

    lab9_2.cpp completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 //Write the function printO() here
 
+void printO(int row_amount, int column_amount){
+	if(row_amount <= 0 || column_amount <= 0){
+		cout << "Invalid input";
+		return;
+	}
+	for(int i = 0; i < row_amount; i++){
+		for(int j = 0; j < column_amount; j++){
+			cout << "O";
+		}
+		cout << "\n";
+	}
+}
+
 int main(){
 	
 	printO(2,2);
