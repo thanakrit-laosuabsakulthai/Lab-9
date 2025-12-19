613441bd84f53ec18201ec8072e71dddf1a16fbf
Thanakrit <thanakrit_la@cmu.ac.th>
2025-12-19 15:41:34 +0700

    lab9_1.cpp completed

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,10 +3,15 @@
 using namespace std;
 
 int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+	//Declare variables
+	double initial_loan, interest_rate, annual_payment;
 
+	cout << "Enter initial loan: ";
+	cin >> initial_loan;
+	cout << "Enter interest rate per year (%): ";
+	cin >> interest_rate;
+	cout << "Enter amount you can pay per year: ";
+	cin >> annual_payment;
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -17,7 +22,28 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
+
+	//Calculate and display table of amortization schedule year by year
+	int year = 1;
+	double prev_balance = initial_loan;
+	while (prev_balance > 0) {
+		double interest = prev_balance * (interest_rate / 100.0);
+		double total = prev_balance + interest;
+		double payment = (total < annual_payment) ? total : annual_payment;
+		double new_balance = total - payment;
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << year; 
+		cout << setw(13) << left << prev_balance; 
+		cout << setw(13) << left << interest; 
+		cout << setw(13) << left << total;
+		cout << setw(13) << left << payment;
+		cout << setw(13) << left << new_balance;
+		cout << "\n";
+
+		prev_balance = new_balance;
+		year++;
+	}
+	/*
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
@@ -28,6 +54,6 @@ int main(){
 	cout << setw(13) << left << 100.0;
 	cout << setw(13) << left << 950.0;
 	cout << "\n";	
-	
+	*/
 	return 0;
 }
